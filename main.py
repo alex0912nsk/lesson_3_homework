9a043bdefba85a1b02b9889e3db7b4486b27de6f
alex0912nsk <alex0912nsk@gmail.com>
2015-12-12 14:03:48 +0600

    decor

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from calculator import Calculator
+
+
 calc = Calculator()
 while True:
     n = 0
     Exp = input("print expression(format: x+y or x-y or x*y or x:y or x/y)")
+
     for i in range(1, len(Exp)):
         if Exp[i] == "+" or Exp[i] == "-" or Exp[i] == "*" or Exp[i] == "/" or Exp[i] == ":":
             n = i
             op = Exp[i]
             break
+
     if n == 0:
         print('No sign of the operation')
         continue
+
     try:
         a = float(Exp[:n])
         b = float(Exp[n+1:])
@@ -29,8 +34,10 @@ while True:
             print('cant zero division')
             continue
         ans = calc.divide(a, b)
+
     if op == ':':
         Exp = str(a) + "/" + str(b)
+
     if ans == calc.evaluate(Exp):
         print(Exp + "=" + str(ans))
     else:
